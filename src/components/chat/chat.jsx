155458adf19ec93cd9d0c3e4cc7756f12b12818e
Samadhan0018 <samadhanpatil0018@gmail.com>
2024-10-01 18:45:59 +0530

    formating and some changes in style of detail and useref function in chat.jsx

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,21 +1,28 @@
-import { useState } from "react"
-import "./chat.css"
-import EmojiPicker from "emoji-picker-react"
-const Chat = () => {
+import { useEffect, useState, useRef } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
+const Chat = () => {
     const [open, setOpen] = useState(false);
     const [text, setText] = useState("");
-    const handleEmoji = e => {
-        setText(prev => prev + e.emoji);
-        setOpen(false)
 
+    // Define the ref
+    const endRef = useRef(null);
+
+    // Scroll to the end of messages when the component loads
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, []);
+
+    const handleEmoji = (e) => {
+        setText((prev) => prev + e.emoji);
+        setOpen(false);
     };
+
     console.log(text);
 
     return (
-
-
-        <div className='chat'>
+        <div className="chat">
             <div className="top">
                 <div className="user">
                     <img src="./avatar.png" alt="" />
@@ -25,7 +32,6 @@ const Chat = () => {
                     </div>
                 </div>
 
-
                 <div className="icons">
                     <img src="./phone.png" alt="" />
                     <img src="./video.png" alt="" />
@@ -33,52 +39,41 @@ const Chat = () => {
                 </div>
             </div>
 
-
-
             <div className="center">
                 <div className="message">
                     <img src="./avatar.png" alt="" />
                     <div className="text">
                         <img src="https://cms.interiorcompany.com/wp-content/uploads/2023/11/simple-house-design-go-for-minimalist.png" alt="" />
-                        <p>Lorem ipsum, dolor sit amet cons
-
-                        </p>
+                        <p>Lorem ipsum, dolor sit amet cons</p>
                         <span>1 min ago</span>
                     </div>
-
                 </div>
+
                 <div className="message-own">
-
-                    <div className="text " id="text">
-                        <p>Lorem ipsum, dolor sit amet consectetur
-
-                        </p>
+                    <div className="text">
+                        <p>Lorem ipsum, dolor sit amet consectetur</p>
                         <span>1 min ago</span>
                     </div>
-
                 </div>
+
                 <div className="message">
                     <img src="./avatar.png" alt="" />
                     <div className="text">
-                        <p>Lorem ipsum, dolor sit amet cons
-
-                        </p>
+                        <p>Lorem ipsum, dolor sit amet cons</p>
                         <span>1 min ago</span>
                     </div>
-
                 </div>
+
                 <div className="message-own">
                     <div className="text">
-                        <p>Lorem ipsum, dolor sit amet con
-
-                        </p>
+                        <p>Lorem ipsum, dolor sit amet con</p>
                         <span>1 min ago</span>
                     </div>
-
                 </div>
+
+                {/* The ref element that the page scrolls to */}
+                <div ref={endRef}></div>
             </div>
-
-
 
             <div className="bottom">
                 <div className="icons">
@@ -86,26 +81,30 @@ const Chat = () => {
                     <img src="./camera.png" alt="" />
                     <img src="./mic.png" alt="" />
                 </div>
-                <input type="text" id="input"
+
+                <input
+                    type="text"
+                    id="input"
                     placeholder="type a message..."
                     value={text}
-                    onChange={e => setText(e.target.value)} />
+                    onChange={(e) => setText(e.target.value)}
+                />
+
                 <div className="emoji">
-                    <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)} />
-                    <div className="picker"><EmojiPicker open={open} onEmojiClick={handleEmoji} />
+                    <img
+                        src="./emoji.png"
+                        alt=""
+                        onClick={() => setOpen((prev) => !prev)}
+                    />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji} />
                     </div>
                 </div>
+
                 <button className="sendButton">Send</button>
             </div>
         </div>
+    );
+};
 
-
-
-
-
-
-
-    )
-}
-export default Chat
-
+export default Chat;
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,75 @@
 import "./detail.css"
 const Detail = () => {
-    return(
-        <div className='detail'>Detail</div>
+    return (
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" className="icon" />
+                <h3>Jane Doe</h3>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" className="icon" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" className="icon" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>privacy and help</span>
+                        <img src="./arrowUp.png" alt="" className="icon" />
+                    </div>
+                </div>
+
+
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" className="icon" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://w0.peakpx.com/wallpaper/957/693/HD-wallpaper-anime-girls-dark-hair-anime-dark-eyes-long-hair-pixiv.jpg" alt="" />
+                            <span>photo_2024_2.png</span>
+                            </div> 
+                            <img src="./download.png" alt="" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://w0.peakpx.com/wallpaper/957/693/HD-wallpaper-anime-girls-dark-hair-anime-dark-eyes-long-hair-pixiv.jpg" alt="" />
+                            <span>photo_2024_2.png</span>
+                            </div> 
+                            <img src="./download.png" alt="" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://w0.peakpx.com/wallpaper/957/693/HD-wallpaper-anime-girls-dark-hair-anime-dark-eyes-long-hair-pixiv.jpg" alt="" />
+                            <span>photo_2024_2.png</span>
+                            </div> 
+                            <img src="./download.png" alt="" />
+                        </div>
+                      
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>shared files</span>
+                        <img src="./arrowDown.png" alt="" className="icon" />
+                    </div>
+                </div>
+            </div>
+            <button>Block user</button>
+
+        </div>
     )
 }
-export default Detail
+
+export default Detail;
