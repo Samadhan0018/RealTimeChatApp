f98802f8c56687b17b371c966a3bc88c29a0a750
Samadhan0018 <samadhanpatil0018@gmail.com>
2024-10-03 00:51:57 +0530

    creating a login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import Login from "./components/login/login";
 import Chat from "./components/chat/chat"
 import Detail from "./components/detail/Detail"
 import List from "./components/list/list"
 const App = () => {
+ const user =false;
   return (
     <div className='container'>
+      {user? (
+       <>
+      
       <List/>
       <Chat/>
       <Detail/>
+      </>
+      ):(
+         <Login/>
+      )}  
+     
     </div>
-  )
-}
+  );
+};
 
 export default App
